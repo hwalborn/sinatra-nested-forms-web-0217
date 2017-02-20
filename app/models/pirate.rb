9bd663cc83eefc6dc58d197a66e12371b15e7248
hwalborn <hwalborn@gmail.com>
2017-02-19 23:27:33 -0500

    Avast ye

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+    @ships = []
+  end
+
+  def self.all
+    @@all
+  end
 end
